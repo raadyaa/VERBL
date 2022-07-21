1f81239176785a4a2ad02dc00c6c6eb88d0a776b
Radya <radyafarrel@gmail.com>
2022-07-21 16:43:05 +0700

    Initial Commit

---
+++ VERBL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VERBL
+//
+//  Created by Radya Farrel on 21/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VERBL/VERBLApp.swift
@@ -0,0 +1,17 @@
+//
+//  VERBLApp.swift
+//  VERBL
+//
+//  Created by Radya Farrel on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct VERBLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
